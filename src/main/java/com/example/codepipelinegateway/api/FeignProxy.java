ff301cc63eea97920ea40b7ded1ba70022aa3254
Hasitha Maduranga <Hasitha.Ma@creativesoftware.com>
2021-07-02 21:49:29 +0530

    test

--- src/main/java/com/example/codepipelinegateway/api/FeignProxy.java
+++ src/main/java/com/example/codepipelinegateway/api/FeignProxy.java
@@ -3,7 +3,7 @@ package com.example.codepipelinegateway.api;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "codepipeline",url = "http://dev-codepilepline-gateway-lb-1931632959.us-east-2.elb.amazonaws.com/spring-codepipeline")
+@FeignClient(name = "codepipeline",url = "http://internal-dev-codepilepline-gateway-lb-1931632959.us-east-2.elb.amazonaws.com/spring-codepipeline")
 public interface FeignProxy {
 
     @GetMapping
